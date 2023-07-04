bbe2714a71372055633b4387371f5102ac9711af
immediatealex <alex.nicholas@immediate.co.uk>
2023-07-04 14:34:02 +0100

    refactor: one-line

--- packages/wdio-config/src/lib/ConfigParser.ts
+++ packages/wdio-config/src/lib/ConfigParser.ts
@@ -177,9 +177,7 @@ export default class ConfigParser {
                 return (values as Readonly<Readonly<Capabilities.Capabilities[]>[]>)
                     .reduce((flattened, current) => [...flattened, ...current], [])
                     .filter((capability, index, array) => {
-                        const firstIndex = array.findIndex(
-                            ({ browserName }) => browserName === capability.browserName,
-                        )
+                        const firstIndex = array.findIndex(({ browserName }) => browserName === capability.browserName)
                         return index === firstIndex
                     })
             },
